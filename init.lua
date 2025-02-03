fdf58e80b6ea2835f5bc2f255970bbea290866cc
Felix Schmelzer <65311773+felixschmelzer@users.noreply.github.com>
2025-02-03 01:53:43 +0100

    enabled auto buffer reloading
    unchanges buffers get reloaded after the focus got back to nvim.

--- init.lua
+++ init.lua
@@ -123,6 +123,10 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
+-- Automatically read file if changed outside of Neovim
+-- useful for git operations, etc.
+vim.opt.autoread = true
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
