1fb0f67bedc607bb6f15aaff541132f99aa7c222
Felix Schmelzer <65311773+felixschmelzer@users.noreply.github.com>
2025-05-22 12:10:28 +0200

    added obsidian plugin

--- init.lua
+++ init.lua
@@ -92,6 +92,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Set conceallevel to show markdown formatting
+vim.opt.conceallevel = 1
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,50 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Felixs Mind',
+      },
+      {
+        name = 'ba',
+        path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Felixs Mind/Uni/BA',
+        strict = true, -- tells obsidian.nvim to use the path as the workspace/vault root
+      },
+    },
+
+    templates = {
+      folder = 'Templates',
+      date_format = '%Y-%m-%d-%a',
+      time_format = '%H:%M',
+    },
+
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart { 'open', url } -- Mac OS
+      -- vim.fn.jobstart({"xdg-open", url})  -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+      -- vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
+
+    -- TODO follow link function
+  },
+}
